4fc659df6bf9afe58c89c997298b8a532bc628a3
Monika <monika.k.rychlicka@gmail.com>
2020-09-05 22:40:40 +0100

    init

---
+++ src/main/java/com/team2/questionme/QuestionMeApplication.java
@@ -0,0 +1,13 @@
+package com.team2.questionme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionMeApplication.class, args);
+    }
+
+}
