08b73f1636cb71c7f14f73ed09b3f3998ec8b5d4
Monika <monika.k.rychlicka@gmail.com>
2020-09-15 21:20:18 +0100

    Hashing password

---
+++ src/main/java/com/team2/questionme/security/PasswordService.java
@@ -0,0 +1,17 @@
+package com.team2.questionme.security;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordService {
+
+    public String hashPassword(String passwordPlaintext) {
+
+        int workload = 12;
+
+        String salt = BCrypt.gensalt(workload);
+        return BCrypt.hashpw(passwordPlaintext, salt);
+
+    }
+}
